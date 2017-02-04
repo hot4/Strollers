ba8e78983be415f4c5afd2dc1bfb5402908b27be
Timothy Ho <tho.nyc26@gmail.com>
2017-02-03 20:48:14 -0500

    Added menu items with toast messages.

--- app/src/main/java/com/example/strollers/strollers/MainActivity.java
+++ app/src/main/java/com/example/strollers/strollers/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.strollers.strollers;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.settings:
+                displayText("Display Settings");
+                return true;
+            case R.id.help:
+                displayText("Display Help");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void displayText(String string) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, string, duration);
+        toast.show();
     }
 }
