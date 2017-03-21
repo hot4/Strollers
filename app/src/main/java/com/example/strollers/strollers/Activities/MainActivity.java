edd7b4913b206cd4d37b8073344b93d8df8d7057
Timothy Ho <tho.nyc26@gmail.com>
2017-03-21 12:52:07 -0400

    GenerateRoute - Established corner cases to handle user denying location permission - Requests location permission based on application launch - Current Location FAB requests location if permission was denied by displaying application setting - User needs to scroll to the permissions section and enable location

--- app/src/main/java/com/example/strollers/strollers/Activities/MainActivity.java
+++ app/src/main/java/com/example/strollers/strollers/Activities/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.strollers.strollers.Activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.strollers.strollers.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,10 +27,15 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
+    @BindView(R.id.main_layout)
+    RelativeLayout mLayout;
+    @BindView(R.id.current_location)
+    FloatingActionButton currLocButton;
     @BindView(R.id.workout)
     Button workoutButton;
 
     private GoogleMap mMap;
+    private static final int PERMISSION_REQUEST_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +49,14 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         workoutButton.setOnClickListener(this);
+
+        currLocButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Snackbar.make(mLayout, "Clicked current location", Snackbar.LENGTH_SHORT).show();
+                currentLocation();
+            }
+        });
     }
 
 
@@ -50,12 +71,57 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        currentLocation();
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    public void currentLocation() {
+        // Check if the Location permission has been granted
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is already available, start camera preview
+            Snackbar.make(mLayout,
+                    "Camera permission is available. Starting preview.",
+                    Snackbar.LENGTH_SHORT).show();
+//            startCamera();
+        } else {
+            // Permission is missing and must be requested.
+            requestLocationPermission();
+        }
+    }
+
+    public void requestLocationPermission() {
+        // Permission has not been denied
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // Display a SnackBar with a button to request the missing permission.
+            Snackbar snackbar = Snackbar.make(mLayout, "Location access is required to display routes based on current location.",
+                    Snackbar.LENGTH_INDEFINITE);
+
+            View snackbarView = snackbar.getView();
+            TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+            textView.setMaxLines(3);
+
+            snackbar.setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Open application setting
+                    Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }
+            }).show();
+        } else {
+            // Request the permission. The result will be received in onRequestPermissionResult().
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
+        }
     }
 
     @Override
