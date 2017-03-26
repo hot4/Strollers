2c9232724e8b03ba67d9ecd12076954e1039ea70
Timothy Ho <tho.nyc26@gmail.com>
2017-03-26 14:03:09 -0400

    GenerateRoute - Added fields place_id, reference, scope, types, and vicinity  for Route model mapping for JSON object

--- app/src/main/java/com/example/strollers/strollers/Models/Route.java
+++ app/src/main/java/com/example/strollers/strollers/Models/Route.java
@@ -1,11 +1,18 @@
 package com.example.strollers.strollers.Models;
 
+import java.util.List;
+
 public class Route {
     private Geometry geometry;
     private String icon;
     private String id;
     private String name;
     private Photos photos;
+    private String place_id;
+    private String reference;
+    private String scope;
+    private List<String> types;
+    private String vicinity;
 
     private String origin;
     private String destination;
@@ -35,6 +42,26 @@ public class Route {
 
     public Photos getPhotos() {
         return photos;
+    }
+
+    public String getPlaceID() {
+        return place_id;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
+
+    public String getVicinity() {
+        return vicinity;
     }
 
     public String getOrigin() {
